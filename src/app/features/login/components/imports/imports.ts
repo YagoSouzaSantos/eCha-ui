3fd74d0adf6e426e95c576fad19bde1bfca4abdf
Yago S Santos <yagossantos32@gmail.com>
2024-08-13 21:15:39 -0300

    [Feat]: Implementação interface sign-in

---
+++ src/app/features/login/components/imports/imports.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
+
+export const MATERIAL = [
+  MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatIconModule,
+  CommonModule,
+]
--- src/app/features/login/components/sign-in/sign-in.component.ts
+++ src/app/features/login/components/sign-in/sign-in.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MATERIAL } from '../imports/imports';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [MATERIAL, FormsModule, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
-
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', Validators.required);
+  hide = true;
 }
+
